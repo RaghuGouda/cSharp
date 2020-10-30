6300d2924d160e9fa5594c0de431ab4988cdb63d
raghu <raghugowda.nm@gmail.com>
2020-10-31 04:06:21 +0530

    c

---
+++ SpecFlowUiAutomation/StepDefinition/WikipediaTitleValidationSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SpecFlowUiAutomation
+{
+    [Binding]
+    public class WikipediaTitleValidationSteps
+    {
+        private readonly IWebDriver _webDriver;
+
+        public WikipediaTitleValidationSteps(ScenarioContext scenarioContext)
+        {
+            _webDriver = scenarioContext["WEB_DRIVER"] as IWebDriver;
+        }
+
+        [Given(@"I have navigated to the ""(.*)"" page on Wikipedia")]
+        public void GivenIHaveNavigatedToThePageOnWikipedia(string subject)
+        {
+            _webDriver.Url = $"https://en.wikipedia.org/wiki/{subject}";
+        }
+
+        [Then(@"the title of the page should be ""(.*)""")]
+        public void ThenTheTitleOfThePageShouldBe(string title)
+        {
+            Assert.Equal(title, _webDriver.Title);
+        }
+    }
+}
---
+++ SpecFlowUiAutomation/pages/WikipediaTitleValidation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SpecFlowUiAutomation.pages
+{
+    class WikipediaTitleValidation
+    {
+
+    }
+}
